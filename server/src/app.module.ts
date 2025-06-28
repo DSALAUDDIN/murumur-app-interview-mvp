d92fa599acd3aa3d2753c6ee76ff4ec7aa2ef3b3
Md Alauddin Mazumder <alauddindatascientist@gmail.com>
2025-06-28 19:41:46 +0600

    feat(auth): scaffold authentication module with controllers, services, JWT strategy, guard, and DTOs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './entities/user.entity';
+import { User } from './auth/entities/user.entity';
 
 @Module({
   imports: [
